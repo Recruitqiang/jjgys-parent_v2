53ecba504fc3fc889b9424e052a551a839f87f59
12910 <1291040624@qq.com>
2023-11-25 11:48:47 +0800

    25

--- service-system/src/main/java/glgc/jjgys/system/service/impl/JjgLqsServiceImpl.java
+++ service-system/src/main/java/glgc/jjgys/system/service/impl/JjgLqsServiceImpl.java
@@ -31,6 +31,42 @@ public class JjgLqsServiceImpl implements JjgLqsService {
     @Autowired
     private JjgHtdService jjgHtdService;
 
+    @Autowired
+    private JjgLqsQlMapper jjgLqsQlMapper;
+
+    @Autowired
+    private JjgLqsSdMapper jjgLqsSdMapper;
+
+    @Autowired
+    private JjgLqsFhlmMapper jjgLqsFhlmMapper;
+
+    @Autowired
+    private JjgLqsHntlmzdMapper jjgLqsHntlmzdMapper;
+
+    @Autowired
+    private JjgLqsSfzMapper jjgLqsSfzMapper;
+
+    @Autowired
+    private JjgLqsLjxMapper jjgLqsLjxMapper;
+
+    @Autowired
+    private JjgLqsLjxqlMapper jjgLqsLjxqlMapper;
+
+    @Autowired
+    private JjgLqsLjxsdMapper jjgLqsLjxsdMapper;
+
+    @Autowired
+    private JjgLqsLjxhntlmMapper jjgLqsLjxhntlmMapper;
+
+    @Autowired
+    private JjgJanumMapper jjgJanumMapper;
+
+    @Autowired
+    private JjgLookProjectPlanMapper jjgLookProjectPlanMapper;
+
+    @Autowired
+    private JjgLjfrnumMapper jjgLjfrnumMapper;
+
     //路桥隧文件导出
     @Override
     public void exportLqs(HttpServletResponse response,String projectname) {
@@ -80,6 +116,42 @@ public class JjgLqsServiceImpl implements JjgLqsService {
                 }
             }
         }
+        //交安的数据
+        List<JjgJanumVo> listja = new ArrayList<>();
+        String[] jasheet = {"单旋","双旋","附着","门洞","单柱","双柱"};
+
+        for (JjgHtd jjgHtd : gethtd) {
+            String lx = jjgHtd.getLx();
+            String htd = jjgHtd.getName();
+            if (lx.contains("交安工程")){
+                for (String s : jasheet) {
+                    JjgJanumVo janum = new JjgJanumVo();
+                    janum.setProname(projectname);
+                    janum.setHtd(htd);
+                    janum.setFbgc("交安工程");
+                    janum.setZb(s);
+                    listja.add(janum);
+                }
+            }
+        }
+        //路基附容的数据
+        List<JjgLjfrnumVo> listljfr = new ArrayList<>();
+        String[] ljsheet = {"支挡","涵洞","通道","小桥"};
+        for (JjgHtd jjgHtd : gethtd) {
+            String lx = jjgHtd.getLx();
+            String htd = jjgHtd.getName();
+            if (lx.contains("路基工程")){
+                for (String s : ljsheet) {
+                    JjgLjfrnumVo janum = new JjgLjfrnumVo();
+                    janum.setProname(projectname);
+                    janum.setHtd(htd);
+                    janum.setFbgc("路基工程");
+                    janum.setZb(s);
+                    listljfr.add(janum);
+                }
+            }
+        }
+
         try {
             String fileName = projectname+"路桥隧数据文件";
             String sheetName1 = "桥梁清单";
@@ -88,20 +160,24 @@ public class JjgLqsServiceImpl implements JjgLqsService {
             String sheetName4 = "混凝土路面及匝道清单";
             String sheetName5 = "收费站清单";
             String sheetName6 = "连接线清单";
-            String sheetName7 = "连接线桥梁清单";
+            /*String sheetName7 = "连接线桥梁清单";
             String sheetName8 = "连接线隧道清单";
-            String sheetName9 = "连接线混凝土路面清单";
+            String sheetName9 = "连接线混凝土路面清单";*/
             String sheetName10 = "项目指标计划清单";
+            String sheetName11 = "交安数量清单";
+            String sheetName12 = "路基附容数量清单";
             ExcelUtil.writeExcelWithSheets(response, null, fileName, sheetName1, new QlVo())
                     .write(null, sheetName2, new SdVo())
                     .write(null, sheetName3, new FhlmVo())
                     .write(null, sheetName4, new HntlmzdVo())
                     .write(null, sheetName5, new SfzVo())
                     .write(null, sheetName6, new LjxVo())
-                    .write(null, sheetName7, new LjxqlVo())
+                    /*.write(null, sheetName7, new LjxqlVo())
                     .write(null, sheetName8, new LjxsdVo())
-                    .write(null, sheetName9, new LjxhntlmVo())
+                    .write(null, sheetName9, new LjxhntlmVo())*/
                     .write(list, sheetName10, new JjgPlaninfoVo())
+                    .write(listja, sheetName11, new JjgJanumVo())
+                    .write(listljfr, sheetName12, new JjgLjfrnumVo())
                     .finish();
         } catch (Exception e) {
             throw new JjgysException(20001,"导出失败");
@@ -109,24 +185,7 @@ public class JjgLqsServiceImpl implements JjgLqsService {
 
     }
 
-    @Autowired
-    private JjgLqsQlMapper jjgLqsQlMapper;
-    @Autowired
-    private JjgLqsSdMapper jjgLqsSdMapper;
-    @Autowired
-    private JjgLqsFhlmMapper jjgLqsFhlmMapper;
-    @Autowired
-    private JjgLqsHntlmzdMapper jjgLqsHntlmzdMapper;
-    @Autowired
-    private JjgLqsSfzMapper jjgLqsSfzMapper;
-    @Autowired
-    private JjgLqsLjxMapper jjgLqsLjxMapper;
-    @Autowired
-    private JjgLqsLjxqlMapper jjgLqsLjxqlMapper;
-    @Autowired
-    private JjgLqsLjxsdMapper jjgLqsLjxsdMapper;
-    @Autowired
-    private JjgLqsLjxhntlmMapper jjgLqsLjxhntlmMapper;
+
 
     @Override
     public void importlqs(MultipartFile file, String projectname) throws IOException {
@@ -214,7 +273,48 @@ public class JjgLqsServiceImpl implements JjgLqsService {
                 }
             }
         }).build();
-        ReadSheet sheetljxql = EasyExcel.readSheet(6).head(LjxqlVo.class).registerReadListener(new ExcelHandler<LjxqlVo>(LjxqlVo.class) {
+        ReadSheet sheetxmjh = EasyExcel.readSheet(6).head(JjgPlaninfoVo.class).registerReadListener(new ExcelHandler<JjgPlaninfoVo>(JjgPlaninfoVo.class) {
+            @Override
+            public void handle(List<JjgPlaninfoVo> dataList) {
+                for(JjgPlaninfoVo jh: dataList)
+                {
+                    JjgPlaninfo jjgjh = new JjgPlaninfo();
+                    BeanUtils.copyProperties(jh,jjgjh);
+                    jjgjh.setProname(projectname);
+                    jjgjh.setCreateTime(new Date());
+                    jjgLookProjectPlanMapper.insert(jjgjh);
+                }
+            }
+        }).build();
+
+        ReadSheet sheetja = EasyExcel.readSheet(7).head(JjgJanumVo.class).registerReadListener(new ExcelHandler<JjgJanumVo>(JjgJanumVo.class) {
+            @Override
+            public void handle(List<JjgJanumVo> dataList) {
+                for(JjgJanumVo ja: dataList)
+                {
+                    JjgJanum jjgja = new JjgJanum();
+                    BeanUtils.copyProperties(ja,jjgja);
+                    jjgja.setProname(projectname);
+                    jjgja.setCreateTime(new Date());
+                    jjgJanumMapper.insert(jjgja);
+                }
+            }
+        }).build();
+
+        ReadSheet sheetlj = EasyExcel.readSheet(8).head(JjgLjfrnumVo.class).registerReadListener(new ExcelHandler<JjgLjfrnumVo>(JjgLjfrnumVo.class) {
+            @Override
+            public void handle(List<JjgLjfrnumVo> dataList) {
+                for(JjgLjfrnumVo lj: dataList)
+                {
+                    JjgLjfrnum jjgljfr = new JjgLjfrnum();
+                    BeanUtils.copyProperties(lj,jjgljfr);
+                    jjgljfr.setProname(projectname);
+                    jjgljfr.setCreateTime(new Date());
+                    jjgLjfrnumMapper.insert(jjgljfr);
+                }
+            }
+        }).build();
+        /*ReadSheet sheetljxql = EasyExcel.readSheet(6).head(LjxqlVo.class).registerReadListener(new ExcelHandler<LjxqlVo>(LjxqlVo.class) {
             @Override
             public void handle(List<LjxqlVo> dataList) {
                 for(LjxqlVo ljxqlVo: dataList)
@@ -258,8 +358,8 @@ public class JjgLqsServiceImpl implements JjgLqsService {
                     jjgLqsLjxhntlmMapper.insert(jjgLjxhntlm);
                 }
             }
-        }).build();
-        excelReader.read(sheetql,sheetsd,sheetfhlm,sheethntlmzd,sheetsfz,sheetljx,sheetljxql,sheetljxsd,sheetljxhntlm);
+        }).build();*/
+        excelReader.read(sheetql,sheetsd,sheetfhlm,sheethntlmzd,sheetsfz,sheetljx,sheetxmjh,sheetja,sheetlj);
         excelReader.finish();
 
     }
